3be62ca8ba35efffcdeb8094b20cc1aca57c5267
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-03-07 22:42:45 -0300

    feat(intents) Intentes
    Added the forst intent

--- app.js
+++ app.js
@@ -1,37 +1,54 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
+// load env variables from the .env file
+require('dotenv-extended').load()
 
 var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
+const port = process.env.port || process.env.PORT || 3978
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(port, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-
-// Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', new builder.MemoryBotStorage())
 
-bot.dialog('/', function (session) {
-    session.send('Você disse isto -> ' + session.message.text);
-});
+// LUIS Dialogs
+const recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL)
+const intents = new builder.IntentDialog({
+    recognizers: [recognizer]
+})
+
+intents.matches('dados-pessoais', () =>{
+    session.send('message test');
+})
+
+intents.onDefault((session, args) => {
+    session.send(`Desculpe, não pude compreender **${session.message.text}**\n\nLembre-se que sou um bot e meu conhecimento é limitado.`)
+})
+
+bot.on('conversationUpdate', (update) => {
+    if (update.membersAdded) {
+        update.membersAdded.forEach( (identity) => {
+            if (identity.id === update.address.bot.id) {
+                bot.loadSession(update.address, (err, session) => {
+                    if(err)
+                        return err;                    
+                    session.send("olá seja bem vindo");
+                })
+            }
+        })
+    }
+})
+
+bot.dialog('/', intents)
