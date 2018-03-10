00b684bf6c4facfb3b269dfd9b21b8ea33e6e9b5
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-03-09 21:36:38 -0300

    feat(intent) dados-pessoai
    Applied intent dados pessoais

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv-extended').load()
 
 var restify = require('restify');
 var builder = require('botbuilder');
-
+var config = require('./config/config');
+ 
 // Setup Restify Server
 const port = process.env.port || process.env.PORT || 3978
 var server = restify.createServer();
@@ -23,18 +24,39 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', new builder.MemoryBotStorage())
 
-// LUIS Dialogs
+// LUIS Dialogs 
 const recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL)
 const intents = new builder.IntentDialog({
     recognizers: [recognizer]
 })
 
-intents.matches('dados-pessoais', () =>{
-    session.send('message test');
-})
+intents.matches('dados-pessoais', (session, args, next) =>{
+    if(args.entities.length > 0){
+        var message = "";
+        args.entities.forEach(function(value, index){
+            if(value.type == "nome"){
+                if(index > 0){
+                    message = message + " e "
+                }
+                message = message +  "Meu nome é **Bot**. ";
+            }else if(value.type == 'idade'){
+                var age = new Date().getTime() - config.birth_date;                
+                if(index > 0){
+                    message = message + " e "
+                }
+                message = message +  `Tenho **${age}** anos/dias de vida (formato em timestamp)`;
+            }
+        });
+    }
+    session.send(message);
+});
+
+intents.matches('saudar', (session) =>{
+    session.send('Olá, em que posso lhe ajudar?');
+});
 
 intents.onDefault((session, args) => {
-    session.send(`Desculpe, não pude compreender **${session.message.text}**\n\nLembre-se que sou um bot e meu conhecimento é limitado.`)
+    session.send(`Desculpe, não pude compreender **${session.message.text}**\n\nLembre-se que sou um bot e meu conhecimento é limitado. - ${config.birth_date}`)
 })
 
 bot.on('conversationUpdate', (update) => {
---
+++ config/config.js
@@ -0,0 +1,3 @@
+var config = module.exports = {};
+
+config.birth_date = 1520468905945;
